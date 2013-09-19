fa967a8f65391e10a0c6d4efafbb95c46b30dc63
Jim Geurts <jim@biacreations.com>
2013-09-19 11:35:46 -0500

    Update to latest mvckickstart nugets. Fix rename script

--- __Rename Utility__/Rename.bat
+++ __Rename Utility__/Rename.bat
@@ -2,23 +2,23 @@
 
 set /p projectName="New Project Name: " %=%
 
-@echo Renaming 'MvcKickstart' to '%projectName%'
-cd ../MvcKickstart/MvcKickstart
+@echo Renaming 'KickstartTemplate' to '%projectName%'
+cd ../KickstartTemplate/KickstartTemplate
 call:renameFiles
-cd ../MvcKickstart/MvcKickstart.Tests
+cd ../KickstartTemplate/KickstartTemplate.Tests
 call:renameFiles
 
 cd ../
 ::@echo %CD%
 ::find/replace in root directory (no recursion to avoid the __Rename Utility__ directory)
-"__Rename Utility__/fart.exe" -- * "MvcKickstart" %projectName%
+"__Rename Utility__/fart.exe" -- * "KickstartTemplate" %projectName%
 
 ::rename files in root directory (no recursion to avoid the __Rename Utility__ directory)
-"__Rename Utility__/fart.exe" -f -- * "MvcKickstart" %projectName%
+"__Rename Utility__/fart.exe" -f -- * "KickstartTemplate" %projectName%
 
 ::need this for renaming directories
-ren MvcKickstart %projectName%
-ren MvcKickstart.Tests %projectName%.Tests
+ren KickstartTemplate %projectName%
+ren KickstartTemplate.Tests %projectName%.Tests
 @echo.
 @echo Done and done!!
 pause
@@ -30,9 +30,9 @@ goto:eof
 :renameFiles
 ::echo %CD%
 ::find/replace in project subdirectories
-"../__Rename Utility__/fart.exe" -r -- * "MvcKickstart" %projectName%
+"../__Rename Utility__/fart.exe" -r -- * "KickstartTemplate" %projectName%
 
 ::rename files in project subdirectories
-"../__Rename Utility__/fart.exe" -r -f -- * "MvcKickstart" %projectName%
+"../__Rename Utility__/fart.exe" -r -f -- * "KickstartTemplate" %projectName%
 
 goto:eof
