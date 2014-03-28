e6a4eb7ec151fa0e8acaa8087207f71c231943f7
Jim Geurts <jim@biacreations.com>
2014-03-28 17:08:20 -0500

    Couple more tweaks with the rename script to fix issues renaming directories.

--- __Rename Utility__/Rename.bat
+++ __Rename Utility__/Rename.bat
@@ -11,17 +11,23 @@ call:renameFiles
 cd ../../KickstartTemplate/KickstartTemplate.Tests
 call:renameFiles
 cd ../
-
+::@echo %CD%
+@echo Renaming root files
 ::@echo %CD%
 ::find/replace in root directory (no recursion to avoid the __Rename Utility__ directory)
-"__Rename Utility__/fart.exe" -- * "KickstartTemplate" %projectName%
+"../__Rename Utility__/fart.exe" -- * "KickstartTemplate" %projectName%
 
 ::rename files in root directory (no recursion to avoid the __Rename Utility__ directory)
-"__Rename Utility__/fart.exe" -f -- * "KickstartTemplate" %projectName%
+"../__Rename Utility__/fart.exe" -f -- * "KickstartTemplate" %projectName%
 
+@echo Renaming 'Directories' from %CD%
 ::need this for renaming directories
 ren KickstartTemplate %projectName%
 ren KickstartTemplate.Tests %projectName%.Tests
+cd ../
+@echo Renaming root directory
+ren KickstartTemplate %projectName%
+
 @echo.
 @echo Done and done!!
 pause
