b3569c9595ce5ef826c4d6a9b7dc16343c6ce96a
LRSummers <lsummers@laughlin.com>
2015-03-05 15:29:12 -0600

    Began duplicating file structure form boilerplate.

---
+++ KickstartTemplate/KickstartTemplate/Content/admin/js/Account/Register.js
@@ -0,0 +1,26 @@
+﻿/*
+@reference ~/Content/admin/js/_lib/jquery
+@reference ~/Content/admin/js/_lib/jquery.validate
+@reference ~/Content/admin/js/_lib/jquery.typewatch
+ */
+
+ $(function() {
+	 $("#Username").typeWatch({
+		wait:750,
+		captureLength: 0,
+		callback: function(value) {
+			$.post(Url.validateUsername, {
+				username: value
+			}).done(function(data) {
+				
+				if (data) {
+					$(".username-valid").show();
+					$(".username-invalid").hide();
+				} else {
+					$(".username-valid").hide();
+					$(".username-invalid").show();
+				}
+			});
+		}
+	 });
+ });
